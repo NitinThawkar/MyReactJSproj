02275477794d62680ea884cf2e2bf245b4384177
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-02-12 09:39:06 +0530

    event 2

---
+++ my-react-js/src/components/ConditionalRendering.jsx
@@ -0,0 +1,3 @@
+export default function ConditionalRendering() {
+  return <h3>hello ConditionalRendering</h3>;
+}
--- my-react-js/src/components/HelloAndEvent.jsx
+++ my-react-js/src/components/HelloAndEvent.jsx
@@ -1,5 +1,8 @@
 const fullName = "Nitin";
 
+{
+  /* same function also defined in hellow component */
+}
 function greeting() {
   console.log("Good morning");
   return "Good morning";
@@ -10,6 +13,12 @@ function sum(a, b) {
 }
 
 function Hello() {
+  /* same function greeting also defined abow */
+  function greeting() {
+    console.log("Good Afternoon");
+    return "Good Afternoon";
+  }
+
   return (
     <div>
       <h3>Hello world first component nitin ! </h3>
@@ -17,7 +26,8 @@ function Hello() {
       <h3> fnction calling in expression : {greeting()}</h3>
       <br></br>
       ------------------Event Function nested function-------------------
-      <button onClick={alert(greeting())}>Click me 1</button>
+      {/* This is a comment in React */}
+      {/*<button onClick={alert(greeting())}>Click me 1</button>*/}
       <br></br>
       ------------------Event Function nested function-------------------
       <button onClick={greeting}>Click me 2</button>
