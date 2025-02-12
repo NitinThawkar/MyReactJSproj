a2e1eb81debc809481ff9b0727a39a4e89ecea97
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-02-12 22:37:16 +0530

    # Handling Multiple Inputs In React
    # Handling Multiple Inputs In React

--- my-react-js/src/components/FormInput.jsx
+++ my-react-js/src/components/FormInput.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 export default function MyFormInput() {
-  const [fullName, setFullName] = useState("");
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState({ firstName: "", lastName: "" });
 
   function handleChange(event) {
     console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function fullNameChange(event) {
     setFullName(event.target.value);
   }
 
@@ -15,8 +20,24 @@ export default function MyFormInput() {
           type="text"
           onChange={(e) => handleChange(e)}
           placeholder="Enter your name"
-          value={fullName}
+          value={name}
         />
+        <hr></hr>
+        FullName: {fullName.firstName} --{fullName.lastName} ---
+        <input
+          onChange={(e) =>
+            setFullName({ ...fullName, firstName: e.target.value })
+          }
+          type="text"
+          value={fullName.firstName}
+        ></input>
+        <input
+          onChange={(e) =>
+            setFullName({ ...fullName, lastName: e.target.value })
+          }
+          type="text"
+          value={fullName.lastName}
+        ></input>
       </form>
     </div>
   );
