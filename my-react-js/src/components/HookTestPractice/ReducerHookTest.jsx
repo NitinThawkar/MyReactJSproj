361af5c7bf942668747571a1081521e9862e35db
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-14 23:38:04 +0530

    useReducer
    useReducer

--- my-react-js/src/components/HookTestPractice/ReducerHookTest.jsx
+++ my-react-js/src/components/HookTestPractice/ReducerHookTest.jsx
@@ -1,8 +1,51 @@
+import { useReducer } from "react";
 
-function ReducerHookTest() {
-  return (
-    <div>ReducerHookTest</div>
-  )
+function MyReducerBasic(state) {
+  return { counter: state.counter + 1 };
 }
 
-export default ReducerHookTest
+function MyReducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function ReducerHookTest() {
+  const [state, dispatch] = useReducer(MyReducerBasic, { counter: 0 });
+  const [mystate, mydispatch] = useReducer(MyReducer, { count: 0 });
+
+  function increment() {
+    dispatch();
+  }
+
+    function decrement() {
+   mydispatch({ type: "decrement" })
+  }
+
+  return (
+    <>
+      {" "}
+      <div>ReducerHookTest</div>
+      <h5>{state.counter}</h5>
+      <button onClick={increment}>Increment</button>
+      
+      <button onClick={() => mydispatch({ type: "increment" })}>
+        Increment
+      </button>
+      <button onClick={() => mydispatch({ type: "decrement" })}>
+        Decrement
+      </button>
+      <button onClick={decrement}>
+        Decrement
+      </button>
+      <h5>{mystate.count}</h5>
+    </>
+  );
+}
+
+export default ReducerHookTest;
