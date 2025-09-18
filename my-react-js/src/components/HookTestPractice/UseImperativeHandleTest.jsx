4529bb733eabc87bed3fdf9312101208fcf378ae
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-18 16:03:08 +0530

    UseImperativeHandleTest
    UseImperativeHandleTest

--- my-react-js/src/components/HookTestPractice/UseImperativeHandleTest.jsx
+++ my-react-js/src/components/HookTestPractice/UseImperativeHandleTest.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { useRef } from 'react'
-import ChildImperativeHandleA from './PropAndStateLift/ChildImperativeHandleA'
+import React from "react";
+import { useRef } from "react";
+import ChildImperativeHandleA from "./PropAndStateLift/ChildImperativeHandleA";
 
 function UseImperativeHandleTest() {
-    const childRef = useRef();
+  const childRef = useRef();
 
-  
+  function triggerChildIncrease() {
+    // we can call child component function using ref
+    childRef.current.increaseVal();
+  }
+
   return (
     <>
-    <div>UseImperativeHandleTest</div>
-    <button onClick={() => childRef.current.callHello()}>Trigger Child Function</button>
-    <ChildImperativeHandleA ref={childRef}></ChildImperativeHandleA>
+      <div>UseImperativeHandleTest</div>
+
+      <button onClick={() => childRef.current.callHello()}>
+        Trigger Child Hello
+      </button>
+
+      <button onClick={triggerChildIncrease}>Trigger Child Increase</button>
+      <ChildImperativeHandleA ref={childRef}></ChildImperativeHandleA>
     </>
-  )
+  );
 }
 
-export default UseImperativeHandleTest
+export default UseImperativeHandleTest;
