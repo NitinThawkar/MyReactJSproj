51c6ed0fcfc8ddcd968a6ff04295e92a36aacd4b
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-19 17:14:37 +0530

    Update UseMemoHookAdvanceMain.jsx

--- my-react-js/src/components/AdvanceHookTestPractic/UseMemoHook/UseMemoHookAdvanceMain.jsx
+++ my-react-js/src/components/AdvanceHookTestPractic/UseMemoHook/UseMemoHookAdvanceMain.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import UseMemoHookAdvanceTest from "./UseMemoHookAdvanceTest";
 
+
 function UseMemoHookAdvanceMain() {
+  const [loopcount, setLoopCount] = useState(50000000);
+
+  function increase() {
+    setLoopCount(loopcount + 1);
+  }
   return (
     <>
       <h3>Use Advance MemoHook Main</h3>
-      <UseMemoHookAdvanceTest ExpLogicCount={50000000} />
+      <UseMemoHookAdvanceTest ExpLogicCount={loopcount} />
+      <button onClick={increase} >Increase Loop Count</button>
     </>
   );
 }
