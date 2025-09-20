dc005e1ce278e6970e8ac594b54f7b08778f5f66
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-20 23:57:55 +0530

    Create UseHookTest.jsx

---
+++ my-react-js/src/components/HookTestPractice/UseHookTest.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UseHookTest() {
+  return (
+    <div>UseHookTest</div>
+  )
+}
+
+export default UseHookTest
+
+
+
+// introduce in react 19
+// use()  we can use it insted of context hook or useeffect hook to fetch data in component
