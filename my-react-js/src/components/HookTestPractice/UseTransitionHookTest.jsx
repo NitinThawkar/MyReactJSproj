6948adccd3dba72823596305e0694378af11098f
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-20 23:47:11 +0530

    Update UseTransitionHookTest.jsx

--- my-react-js/src/components/HookTestPractice/UseTransitionHookTest.jsx
+++ my-react-js/src/components/HookTestPractice/UseTransitionHookTest.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useTransition } from "react";
 export default function UseTransitionHookTest() {
-
+  //like parallel rendering/prosessing
   // useTransition returns a stateful value for pending state and a function to start the transition.
   // It is used to mark some state updates as non-urgent.
   // This will make the app more responsive by avoiding blocking the UI during heavy rendering tasks.
