11d0f8544bf0e22c340eec0dc9aabbd83a2dd8f4
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-20 22:49:08 +0530

    Update UseFormStatusHookTest.jsx

--- my-react-js/src/components/HookTestPractice/UseFormStatusHookTest.jsx
+++ my-react-js/src/components/HookTestPractice/UseFormStatusHookTest.jsx
@@ -7,3 +7,12 @@ function useFormStatusHookTest() {
 }
 
 export default useFormStatusHookTest
+
+//useFormState has been renamed to React.useActionState. 
+// Please update UseFormStateHookTest to use React.useActionState.
+
+// useFormState + useFormStatus = useActionState
+// useFormState and  useFormStatus depricated in react 19 use useActionState
+// use useActionState is the new combined hook for form state and status.
+// useFormState has been renamed to React.useActionState.
+// useFormStatus has been renamed to React.useActionStatus.
