ad6f27d3b221bdcf964a6e6630caae3295405e29
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-20 22:24:47 +0530

    useFormState
    useFormState

--- my-react-js/src/components/HookTestPractice/Common/FormActionTest.jsx
+++ my-react-js/src/components/HookTestPractice/Common/FormActionTest.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-const FormActionTest = (formData) => {
+const FormActionTest = (prevState, formData) => {
   console.log("form data", formData);
   const name = formData.get("name");
   const age = formData.get("age");
   const email = formData.get("email");
   const save = formData.get("save");
+
   console.log("name", name, "age", age, "email", email, "save", save);
   alert(`name: ${name} age: ${age} email: ${email} save: ${save}`);
+
+  if (name) {
+    return { message: "Student Name=" + name };
+  } else {
+    return { message: "Student name is not inserted" };
+  }
 };
 
 export default FormActionTest;
-
--- my-react-js/src/components/HookTestPractice/UseFormStateHookTest.jsx
+++ my-react-js/src/components/HookTestPractice/UseFormStateHookTest.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import FormActionTest from './Common/FormActionTest';
+import { useFormState } from 'react-dom';
 const UseFormStateHookTest = () => {
+
+  const initialState = {  message:null, }
+
+  const { formState, handleChange } = useFormState(FormActionTest,initialState);
+  console.log("formState", formState);
   return (
     <>
       <h2> useFormState Hook UseFormStateHookTest student information</h2>
-      <form method="post" action={FormActionTest}>
+      <form method="post" action={handleChange}>
         <input type="text" name="name" placeholder="Enter Name" /> <br />
         <input type="text" name="age" placeholder="Enter Age" /> <br />
         <input type="text" name="email" placeholder="Enter Email" /> <br />
@@ -12,6 +18,7 @@ const UseFormStateHookTest = () => {
           Submit
         </button>
       </form>
+  {formState && formState.message && <p>{formState.message}</p>}
     </>
   );
 }
